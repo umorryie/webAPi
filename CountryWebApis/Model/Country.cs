898a90e8af7bcf1cb4d5b743675bc8fed73e7979
UMOR YE <matej.pesjak@adacta-fintech.com>
2020-11-02 19:21:59 +0100

    Added first models

---
+++ CountryWebApis/Model/Country.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.OData.Edm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryWebApis.Model
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public Date Date { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
